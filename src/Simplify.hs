09e3758c5138c4ffa44428a431297b98d6133adc
Raymond <raymond.g.chen.22@dartmouth.edu>
2020-02-27 10:50:45 -0500

    Fixed simplfy functions

--- src/Simplify.hs
+++ src/Simplify.hs
@@ -4,39 +4,27 @@ import DataTypes
 
 type Equation = (Expression,Expression)
 
--- calculate :: [Law] -> Expression -> Calculation 
--- calculate laws e = Calc e (manyStep rws e)   
---     where rws e = [Step name f | Law name eq <- laws, f <- rewrites eqn e] 
+calculate :: [Law] -> Expression -> Calculation 
+calculate laws e = Calc e (manyStep rws e)   
+    where rws e = [Step name f | Law name eq <- laws, f <- rewrites eqn e] 
 
--- manyStep :: (Expression -> [Step]) -> Expression -> [Step]
--- manyStep rws e  
---  = case steps of
---     [] -> []        
---     (o@(Step _ e) : _) -> o:manyStep rws e   
---  where steps = rws e 
-
--- rewrites :: Equation -> Expression -> [Expression] 
--- -- rewrites eqn (Compose as)
--- --    = map Compose (rewriteSeg eqn as ++ anyOne (rewritesA eqn) as) 
-
--- rewritesA eqn (Var v) = [] 
--- rewritesA eqn (Con k es) 
---   = map (Con k) (anyOne (rewrites eqn) es)
-
--- rewritesSeg :: Equation -> [Expression] -> [[Expression]]
--- rewritesSeg (e1,e2) as  
---  = [as1 ++ deCompose (apply subst e2) ++ as3      
---    | (as1,as2,as3) <- split3 as      
---    , subst <- match (e1, Compose as2) ]
-
-
+manyStep :: (Expression -> [Step]) -> Expression -> [Step]
+manyStep rws e  
+ = case steps of
+    [] -> []        
+    (o@(Step _ e) : _) -> o:manyStep rws e   
+ where steps = rws e 
 
 rewrites :: Equation -> Expression -> [Expression]
-rewrites eqn (BinOp oper expr1 expr2) = [BinOp oper a' b | a' <- rewrites eqn expr1] ++ [BinOp oper a b' | b' <- rewrites eqn expr2]
+rewrites eqn (BinOp oper expr1 expr2) = (rewritesHelper eqn (BinOp oper expr1 expr2))  ++ [BinOp oper a' expr2 | a' <- rewrites eqn expr1] ++ [BinOp oper expr1 b' | b' <- rewrites eqn expr2]
 
-rewrites eqn (UnOp oper expr) = [UnOp oper (rewrites eqn expr1)]
+rewrites eqn (UnOp oper expr) = (rewritesHelper eqn (UnOp oper expr)) ++ [UnOp oper a' | a' <- (rewrites eqn expr)]
 
-rewrites eqn (Var v) = match apply v
+rewrites eqn x = rewritesHelper eqn x
+
+
+
+rewritesHelper (e1, e2) exp = [apply sub e2 | sub <- match e1 exp]
 
 
 match :: Expression -> Expression -> [Subst]
@@ -67,28 +55,21 @@ type VarName = String
 unitSub :: VarName -> Expression -> Subst 
 unitSub v e = [(v,e)]
 
--- apply :: Subst -> Expression -> Expression
--- apply sub (Compose as) = Compose (concatMap (applyA sub) as) 
-
--- applyA :: Subst -> Expression -> [Expression] 
--- applyA sub (Var v) = deCompose (binding sub v) 
--- applyA sub (Con k es) = [Con k (map (apply sub) es)] 
-
 binding :: Subst -> VarName -> Expression 
 binding ((v',e):sub) v | v' == v = e                                  
     | otherwise = binding sub v 
 binding [] v = error "Could not find binding"
 
--- combine :: [[Subst]] -> [Subst]
--- combine = filterUnifiable . cp b
--- filterUnifiable = concatMap unifyAll 
--- unifyAll :: [Subst] -> [Subst] 
--- unifyAll = foldr f []   
---     where f sub subs = concatMap (unify sub) subs
+combine :: [[Subst]] -> [Subst]
+combine = filterUnifiable . cp b
+filterUnifiable = concatMap unifyAll 
+unifyAll :: [Subst] -> [Subst] 
+unifyAll = foldr f []   
+    where f sub subs = concatMap (unify sub) subs
 
--- unify :: Subst -> Subst -> [Subst] 
--- unify s1 s2 = if compatible s1 s2 then [s1 ++ s2] else [] 
+unify :: Subst -> Subst -> [Subst] 
+unify s1 s2 = if compatible s1 s2 then [s1 ++ s2] else [] 
 
--- compatible :: Subst -> Subst -> Bool 
--- compatible sub1 sub2   
---     = and [e1 == e2 | (v1, e1) <- sub1, (v2, e2) <-sub, v1==v2]
+compatible :: Subst -> Subst -> Bool 
+compatible sub1 sub2   
+    = and [e1 == e2 | (v1, e1) <- sub1, (v2, e2) <-sub, v1==v2]
