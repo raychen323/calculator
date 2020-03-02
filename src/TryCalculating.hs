07ff576d1d66020c5a8ef0415a93ab14c18edbf3
Raymond <raymond.g.chen.22@dartmouth.edu>
2020-03-02 17:02:26 -0500

    COmmented out plus law since it's not working

--- src/TryCalculating.hs
+++ src/TryCalculating.hs
@@ -6,7 +6,7 @@ import Text.Megaparsec
 
 
 test = calculate laws output where
-    Right output = parse parseExpression "" "derive(5*x)" --"derive (sin(x)*(x^(1/2)))"
+    Right output = parse parseExpression "" "derive(3*x + 7*x)" --"derive (sin(x)*(x^(1/2)))"
 
 
 laws = [
@@ -26,7 +26,7 @@ laws = [
     ,   Law "identity" (BinOp "Mult" (Var "x") (Con 1)) (Var "x")
     ,   Law "identity" (BinOp "Sum" (Var "x") (Con 0)) (Var "x")
     ,   Law "identity" (BinOp "Sum" (Con 0) (Var "x")) (Var "x")
-    ,   Law "plus" (BinOp "Sum" (Var "x") (Var "x")) (BinOp "Mult" (Con 2) (Var "x"))
+    -- ,   Law "plus" (BinOp "Sum" (Var "x") (Var "x")) (BinOp "Mult" (Con 2) (Var "x"))
     ,   Law "self div" (BinOp "Div" (Var "x") (Var "x")) (Con 1)
     ,   Law "self div" (BinOp "Mult" (Var "x") (BinOp "Div" (Con 1) (Var "x"))) (Con 1)
     ,   Law "self div" (BinOp "Mult" (BinOp "Div" (Con 1) (Var "x")) (Var "x")) (Con 1)
