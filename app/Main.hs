ebcaabd18216d7095fb5d42a4c7625868103ee07
Austin Zhang <austinzhang1018@gmail.com>
2020-03-03 12:48:31 -0500

    added main for running program

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
+import TryCalculating
+import Presentable
+import Simplify
+import Parse
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+solve eq = calculate laws output where
+  Right output = parse parseExpression "" eq
 
 main :: IO ()
-main = putStrLn("hi")
+main = do  
+    putStrLn "Please type in your equation"  
+    eq <- getLine
+    putStrLn(show(pretty(solve(eq))))
+    main
