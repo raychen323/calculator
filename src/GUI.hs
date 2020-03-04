89ce59d1218b304b68ad7e223dcc3bcbd044a1ae
Austin Zhang <austinzhang1018@gmail.com>
2020-03-04 17:27:24 -0500

    Finished GUI

--- src/GUI.hs
+++ src/GUI.hs
@@ -1,28 +1,73 @@
+-- Referenced https://www.stackbuilders.com/tutorials/haskell/gui-application/ for button creation, basic setup, and signals
+-- Referenced https://hackage.haskell.org/package/gtk3-0.14.6 for everything else
+
+-- commenting is really extensive as many calls are to library functions so we over-explain a bit
+
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.IORef
 import Graphics.UI.Gtk hiding (Action, backspace)
+import Laws
+import Presentable
+import Simplify
+import Parse
+import Text.Megaparsec
+import Text.Megaparsec.Char()
+import DataTypes
 
-mkBtn :: String -> IO Button
-mkBtn label = do
+-- Same function as in main for running our solver
+-- Copy pasted here so our command line program is decoupled from our GUI
+solve :: String -> Calculation
+solve eq = calculate laws output where
+  Right output = parse parseExpression "" eq
+
+-- function for making our equals button
+mkBtn :: String -> Entry -> TextView -> Entry -> IO Button
+mkBtn label entry steps solView = do
+  -- create new button and set it's label
   btn <- buttonNew
   set btn [ buttonLabel := label ]
+  -- when we click the button run the following
+  btn `on` buttonActivated $ do
+    -- get the user input
+    input <- entryGetText entry
+    -- plug it into our calculator
+    let calculation = solve input
+    -- display the steps
+    editTextDisplay steps (finalOutput(pretty(calculation)))
+    -- display the final answer
+    entrySetText solView (solution calculation)
+    -- reset the entry field to empty string
+    entrySetText entry ""
+  -- return the button we made
   return btn
 
+-- function for building our text display for showing our steps
+createTextDisplay :: String -> IO TextView
 createTextDisplay text = do
+  -- make a new textview
   textView <- textViewNew
+  -- make a new buffer
   buffer <- textBufferNew Nothing
+  -- create our iterator within our buffer
   textIter <- textBufferGetIterAtOffset buffer (-1)
+  -- write to our buffer
   textBufferInsert buffer textIter text
+  -- set display to show our buffer
   set textView [ textViewEditable := False
-               , textViewBuffer   := buffer
-              ]
+               , textViewBuffer   := buffer ]
   return textView
 
+-- function for editing our text display for showing our steps
+editTextDisplay :: TextView -> String -> IO()
 editTextDisplay textView text = do
+  -- create our buffer
   buffer <- textBufferNew Nothing
+  -- create our iterator within our buffer
   textIter <- textBufferGetIterAtOffset buffer (-1)
+  -- write to our buffer
   textBufferInsert buffer textIter text
+  -- set display to show our buffer
   set textView [ textViewBuffer := buffer ]
 
 main :: IO ()
@@ -33,7 +78,7 @@ main = do
   set window [ windowTitle         := "Calculator"
              , windowDefaultWidth  := 400
              , windowResizable     := False
-             , windowDefaultHeight := 500 ]
+             , windowDefaultHeight := 800 ]
 
   -- create layout
   grid <- gridNew
@@ -42,29 +87,27 @@ main = do
 
   -- create display for solution
   solution <- entryNew
-  set solution [ entryEditable := False
-              , entryXalign   := 1 -- makes contents right-aligned
-              , entryText     := "0" ]
+  set solution [ entryEditable := False]
 
   -- create display for the steps the solver takes
   steps <- createTextDisplay "Steps will show here\n when you input an equation"
+
   -- create entry for user input
   entry <- entryNew
   set entry [ entryEditable   := True
-            , entryText       := "Input"]
+            , entryPlaceholderText := (Just "Input")]
 
   -- bind our displays to our grid layout
-  let attach x y w h item = gridAttach grid item x y w h
-  attach 0 0 5 1 solution
-  mkBtn "="   >>= attach 4 3 1 1
-  attach 0 1 5 2 steps
-  attach 0 3 5 1 entry
-  
+  let attach  x   y   w   h   item = gridAttach grid item x y w h
+  attach      0   0   6   1   solution
+  attach      0   1   6   19  steps
+  attach      0   20  5   1   entry
+  mkBtn "=" entry steps solution  >>= attach 5 20 1 1
   -- bind our grid to our display and display all of our widgets
   containerAdd window grid
+  widgetShowAll window
   -- exits program when window is closed
   window `on` deleteEvent $ do
     liftIO mainQuit
     return False
-  widgetShowAll window
   mainGUI
